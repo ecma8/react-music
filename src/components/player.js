325d7b56b4e6d876a32370cc8e70cb99f36bc3f0
ecma8 <756932563@qq.com>
2017-10-13 19:23:55 +0800

    remove MP3

--- src/components/player.js
+++ src/components/player.js
@@ -31,7 +31,6 @@ class Player extends Component{
 
 	}
 	formatTime = (time) =>{
-		console.log(time);
 		time = Math.floor(time);
 		let miniute = Math.floor(time / 60);
 		let seconds = Math.floor(time % 60);
